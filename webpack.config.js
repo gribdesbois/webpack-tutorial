3686289c19f2b0f5ba787ffc93376c0022a17c6e
gribdesbois <gribdesbois@gmail.com>
2021-06-23 17:13:00 +0200

    font handling

--- webpack.config.js
+++ webpack.config.js
@@ -6,17 +6,24 @@ module.exports = {
         filename: 'bundle.js',
         path: path.resolve(__dirname, 'dist'),
     },
-    module: {}
+    module: {
         rules: [
             {
-                test: /\.css$/,
+                test: /\.css$/i,
                 use: [
                     'style-loader',
                     'css-loader',
                 ],
             },
+            {
+                test: /\.(png|svg|jpg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.(woff|woff2|eot|ttf|otf)$/i,
+                type: 'asset/resource',
+            },
         ],
-},
-        
+    },      
 };
 
